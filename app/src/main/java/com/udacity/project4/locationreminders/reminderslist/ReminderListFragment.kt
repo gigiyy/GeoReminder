449c6041993366da44196ffd7823fd4987fc61f0
Guixin Zhu <guixin.zhu@yahoo.com>
2021-10-19 21:46:02 +0900

    add user login

--- app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
@@ -1,9 +1,14 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import com.firebase.ui.auth.AuthUI
 import com.udacity.project4.R
+import com.udacity.project4.authentication.AuthenticationActivity
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentRemindersBinding
@@ -13,6 +18,10 @@ import com.udacity.project4.utils.setup
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ReminderListFragment : BaseFragment() {
+    companion object {
+        const val TAG = "ReminderListFragment"
+    }
+
     //use Koin to retrieve the ViewModel instance
     override val _viewModel: RemindersListViewModel by viewModel()
     private lateinit var binding: FragmentRemindersBinding
@@ -43,6 +52,18 @@ class ReminderListFragment : BaseFragment() {
         binding.addReminderFAB.setOnClickListener {
             navigateToAddReminder()
         }
+        _viewModel.authSate.observe(viewLifecycleOwner, Observer { state ->
+            when (state) {
+                AuthenticationSate.AUTHENTICATED -> Log.i(TAG, "User Logged in")
+                AuthenticationSate.UNAUTHENTICATED -> {
+                    val intent = Intent(
+                        requireActivity().applicationContext,
+                        AuthenticationActivity::class.java
+                    )
+                    startActivity(intent)
+                }
+            }
+        })
     }
 
     override fun onResume() {
@@ -71,7 +92,7 @@ class ReminderListFragment : BaseFragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.logout -> {
-//                TODO: add the logout implementation
+                AuthUI.getInstance().signOut(requireContext())
             }
         }
         return super.onOptionsItemSelected(item)
--- app/src/main/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModel.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModel.kt
@@ -2,12 +2,18 @@ package com.udacity.project4.locationreminders.reminderslist
 
 import android.app.Application
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
 import com.udacity.project4.base.BaseViewModel
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
+import com.udacity.project4.utils.FirebaseUserLiveData
 import kotlinx.coroutines.launch
+
+enum class AuthenticationSate {
+    AUTHENTICATED, UNAUTHENTICATED, INVALID_AUTHENTICATION
+}
 
 class RemindersListViewModel(
     app: Application,
@@ -15,6 +21,14 @@ class RemindersListViewModel(
 ) : BaseViewModel(app) {
     // list that holds the reminder data to be displayed on the UI
     val remindersList = MutableLiveData<List<ReminderDataItem>>()
+
+    val authSate = FirebaseUserLiveData().map { user ->
+        if (user != null) {
+            AuthenticationSate.AUTHENTICATED
+        } else {
+            AuthenticationSate.UNAUTHENTICATED
+        }
+    }
 
     /**
      * Get all the reminders from the DataSource and add them to the remindersList to be shown on the UI,
